6f69fd139cc6f8e6a5f92f9a44f15ba43bbd4373
Kent Collins <1175379+kentcollins@users.noreply.github.com>
2020-09-29 08:43:46 +0100

    added comments to starter code

--- LogMessage.java
+++ LogMessage.java
@@ -1,14 +1,19 @@
 public class LogMessage {
 
+   // two fields maintain instance data
    private String machineId;
    private String description;
 
+   // use the constructor to initialize the fields
+   // with the proper data
    public LogMessage(String message) {
-   // to be implemented as part a
+      // to be implemented as part a
    }
    
+   // given a keyword, determine whether it appears
+   // as described in the problem
    public boolean containsWord(String keyword) {
-   // to be implemented as part b
+      // to be implemented as part b
    }
 
    public String getMachineId() {
